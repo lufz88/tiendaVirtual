d74a92b6c2a24fffdee289ca413eaa2238308555
lufz <lucasfrazzetta@gmail.com>
2023-02-09 20:56:00 -0300

    codigo para mostrar los productos en el HTML

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const productContainer = document.getElementById("container"); 
+
 let eleccion;
 let total = 0;
 let cuotas;
@@ -23,50 +25,59 @@ let producto6 = new Producto("Buzo", 8000, 6);
 let productos = [];
 productos.push(producto1, producto2, producto3, producto4, producto5, producto6);
 
-function iniciar() {
-    mostrarProductos(productos)
-    elegirProducto();
-    comprarMas();    
-    cuotas = prompt("Ingrese la cantidad de cuotas");
-    calculadorCuotas(total, cuotas);
-    alert(`Pagará $${total} en ${cuotas} cuotas de $${cuota}`);
-}
+productos.forEach(producto => {
+    let tarjeta = document.createElement("div");
+    tarjeta.innerHTML = `
+        <p>${producto.codigo}- ${producto.nombre}</p>
+        <p>${producto.precio}</p>
+    `
+    productContainer.appendChild(tarjeta);
+});
 
-function calculadorCuotas(total, cuotas) {
-    if (total > 0 && cuotas > 0) {
-        cuota = (total / cuotas).toFixed(2);
-        return;
-    } else {
-        alert(`Ingrese un precio y/o cuotas válidos`);
-        iniciar();
-    }
-}
+// function iniciar() {
+//     mostrarProductos(productos)
+//     elegirProducto();
+//     comprarMas();    
+//     cuotas = prompt("Ingrese la cantidad de cuotas");
+//     calculadorCuotas(total, cuotas);
+//     alert(`Pagará $${total} en ${cuotas} cuotas de $${cuota}`);
+// }
 
-function mostrarProductos(arr) { //puedo usar forEach
-    let listaProductos = [];
-    productos.forEach(producto => {
-        let mostrar = `${producto.codigo}- ${producto.nombre}`; 
-        listaProductos.push(mostrar); })
-    stringProductos = listaProductos.join("; ");
-}
+// function calculadorCuotas(total, cuotas) {
+//     if (total > 0 && cuotas > 0) {
+//         cuota = (total / cuotas).toFixed(2);
+//         return;
+//     } else {
+//         alert(`Ingrese un precio y/o cuotas válidos`);
+//         iniciar();
+//     }
+// }
+
+// function mostrarProductos(arr) { //puedo usar forEach
+//     let listaProductos = [];
+//     productos.forEach(producto => {
+//         let mostrar = `${producto.codigo}- ${producto.nombre}`; 
+//         listaProductos.push(mostrar); })
+//     stringProductos = listaProductos.join("; ");
+// }
 
 
-function elegirProducto() {
-    eleccion = prompt(`Elija el producto deseado: ${stringProductos}`)
-    if (eleccion > 0 && eleccion <= productos.length) {
-        total += productos[eleccion - 1].precio;
-    } else {
-        alert("Elija un número válido");
-        elegirProducto();
-    }
-}
+// function elegirProducto() {
+//     eleccion = prompt(`Elija el producto deseado: ${stringProductos}`)
+//     if (eleccion > 0 && eleccion <= productos.length) {
+//         total += productos[eleccion - 1].precio;
+//     } else {
+//         alert("Elija un número válido");
+//         elegirProducto();
+//     }
+// }
 
-function comprarMas() {
-    pregunta = prompt("¿Desea otro producto?").toUpperCase();
-    while(pregunta === "SI") {
-        elegirProducto();
-        pregunta = prompt("¿Desea otro producto?").toUpperCase();
-    }
-}
+// function comprarMas() {
+//     pregunta = prompt("¿Desea otro producto?").toUpperCase();
+//     while(pregunta === "SI") {
+//         elegirProducto();
+//         pregunta = prompt("¿Desea otro producto?").toUpperCase();
+//     }
+// }
 
-iniciar()
+// iniciar()
