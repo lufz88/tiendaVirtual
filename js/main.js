37b6191e4897cf7e30496c86246659c295a40c35
lufz <lucasfrazzetta@gmail.com>
2023-02-14 20:47:54 -0300

    Vista de los productos en la tienda

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const productContainer = document.getElementById("container"); 
+const cardsContainer = document.getElementById("cards-container"); 
 
 let eleccion;
 let total = 0;
@@ -8,30 +8,39 @@ let pregunta;
 let stringProductos;
 
 class Producto {
-    constructor(nombre, precio, codigo) {
+    constructor(nombre, precio, codigo, foto) {
         this.nombre = nombre;
         this.precio = precio;
         this.codigo = codigo;
+        this.foto = foto;
     }
 }
 
-let producto1 = new Producto("Pantalón", 5000, 1);
-let producto2 = new Producto("Remera", 4000, 2);
-let producto3 = new Producto("Zapatillas", 10000, 3);
-let producto4 = new Producto("Guantes", 1000, 4);
-let producto5 = new Producto("Gorro", 2000, 5);
-let producto6 = new Producto("Buzo", 8000, 6);
+let producto1 = new Producto("Pantalón", 5000, 1, "./img/pantalon.jpg");
+let producto2 = new Producto("Remera", 4000, 2, "./img/remera.jpg");
+let producto3 = new Producto("Zapatillas", 10000, 3, "./img/zapatillas.jpg");
+let producto4 = new Producto("Guantes", 1000, 4, "./img/guantes.jpg");
+let producto5 = new Producto("Gorro", 2000, 5, "./img/gorro.jpg");
+let producto6 = new Producto("Buzo", 8000, 6, "./img/buzo.jpg");
 
 let productos = [];
 productos.push(producto1, producto2, producto3, producto4, producto5, producto6);
 
 productos.forEach(producto => {
     let tarjeta = document.createElement("div");
+    tarjeta.className = "product-card";
     tarjeta.innerHTML = `
-        <p>${producto.codigo}- ${producto.nombre}</p>
-        <p>${producto.precio}</p>
+        <img src="${producto.foto}">
+            <div class="product-info">
+                <p>$ ${producto.precio}</p>
+                <p>$ ${producto.nombre}</p>
+                <figure>
+                <img src="./img/icons/bt_add_to_cart.svg" alt="logo-carrito">
+                </figure> 
+            </div>
+                           
     `
-    productContainer.appendChild(tarjeta);
+    cardsContainer.appendChild(tarjeta);
 });
 
 // function iniciar() {
